5c6832a6d6b2c1d5771768617da4daf15de3b242
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-12-31 17:32:34 +0800

    feat: Tour of Go basics

---
+++ tour_of_go/README.md
@@ -0,0 +1,3 @@
+# Tour_of_Go
+
+Document some basics that I learnt while going through the [Tour of Go](https://go.dev/tour/welcome/1).
---
+++ tour_of_go/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	b := 1.5
+	var c, max = 20, 30
+	fmt.Printf("Hello,%d %f %d %d Arch Linux!\n", a, b, c, max)
+}
+
